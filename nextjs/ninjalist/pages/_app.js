6e6cd6e80f66a8039a4ea7c87530402af3fde3ce
timothyAgevi <timothyagevi@gmail.com>
2022-04-16 21:30:16 +0300

    import Layout to _app.js

--- nextjs/ninjalist/pages/_app.js
+++ nextjs/ninjalist/pages/_app.js
@@ -1,7 +1,11 @@
+import Layout from '../comps/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <Layout></Layout>
+  )
 }
 
 export default MyApp
