ddae7f34589f6a26f298ca2eed0488f4e3999a49
timothyAgevi <timothyagevi@gmail.com>
2022-04-16 20:29:45 +0300

    add footer

--- nextjs/ninjalist/pages/index.js
+++ nextjs/ninjalist/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../comps/footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
@@ -9,6 +10,8 @@ export default function Home() {
       <Navbar/>
       <h1> Homepage</h1>
       <p>lorem nfgjlkgjsd;fS;lgjklkjlkfnkl  </p>
+      <p> loremfkgjdbfjksfklasj</p>
+      <Footer/>
     </div>
   )
 }
