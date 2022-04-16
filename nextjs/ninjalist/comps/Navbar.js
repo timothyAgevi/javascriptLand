16c99ac6f12d51ec258eeef636cf2ef4f1cf0ad5
timothyAgevi <timothyagevi@gmail.com>
2022-04-16 21:15:07 +0300

    comma before last Link

--- nextjs/ninjalist/comps/Navbar.js
+++ nextjs/ninjalist/comps/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
             </div>
            
             <Link href="/"><a> Home</a></Link>
-            <Link href="/about"><a>About</a></Link>
+            <Link href="/about"><a>About</a></Link>,
             <Link href="/ninjas"><a>Ninja Listings</a></Link>
         </nav>
      );
