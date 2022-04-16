a1d2196889f153ae8bb04e07fa3122a08ddeb3de
timothyAgevi <timothyagevi@gmail.com>
2022-04-16 21:17:53 +0300

    commented out link on ninjas

--- nextjs/ninjalist/pages/ninjas/index.js
+++ nextjs/ninjalist/pages/ninjas/index.js
@@ -1,6 +1,8 @@
+// import Navbar from "../../comps/Navbar";
 const Ninjas = () => {
     return ( 
         <div>
+            {/* <Navbar/> */}
            <h1>All Ninjas</h1> 
         </div>
      );
