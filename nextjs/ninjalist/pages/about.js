e272c2f37e9cd8504f7490ab31b80bc438b24548
timothyAgevi <timothyagevi@gmail.com>
2022-04-16 23:57:27 +0300

    metadata for about page

--- nextjs/ninjalist/pages/about.js
+++ nextjs/ninjalist/pages/about.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
 const About = () => {
     return (
         <>
+      <Head>
+        <title> Ninja List |About</title>
+        <meta name='keywords' content ="ninjas"/>
+     </Head>
         <div>
             <h1>About</h1>
            <p>lorem djfnknsklnjnzknkl</p>
