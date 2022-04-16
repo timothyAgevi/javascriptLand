12bffac6ee3564c0959f0fec6510414c97c6d8bd
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 00:12:40 +0300

    add getStaticProps

--- nextjs/ninjalist/pages/ninjas/index.js
+++ nextjs/ninjalist/pages/ninjas/index.js
@@ -1,4 +1,7 @@
-// import Navbar from "../../comps/Navbar";
+//getStaticProps only runs at build time,dont write browser code here
+export const getStaticProps=async ()=>{
+    const res= await fetch('https://jsonplaceholder.typicode.com/users')
+} 
 const Ninjas = () => {
     return ( 
         <div>
