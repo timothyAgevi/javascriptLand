98b7b9a12ded138f9e3edb92f2bdad0ec76ee585
timothyAgevi <timothyagevi@gmail.com>
2022-04-16 23:54:00 +0300

    fragment for about page

--- nextjs/ninjalist/pages/about.js
+++ nextjs/ninjalist/pages/about.js
@@ -1,10 +1,12 @@
 const About = () => {
-    return ( 
+    return (
+        <>
         <div>
             <h1>About</h1>
            <p>lorem djfnknsklnjnzknkl</p>
            <p> lorem jdjd</p>
         </div>
+        </> 
      );
 }
  
