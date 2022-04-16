d962d14c1b7e28e6d7a5c148c6807835681d89eb
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 00:20:06 +0300

    data from json

--- nextjs/ninjalist/pages/ninjas/index.js
+++ nextjs/ninjalist/pages/ninjas/index.js
@@ -1,9 +1,15 @@
 //getStaticProps only runs at build time,dont write browser code here
 export const getStaticProps=async ()=>{
     // res object of the fetch request
-    const res= await fetch('https://jsonplaceholder.typicode.com/users')
+    const res= await fetch('https://jsonplaceholder.typicode.com/users');
+    //data from json 
+    const data= await res.json();
+    // to use this data inside component
+    return {
+        props:{ninjas:data} // props property is a object
+    }
 } 
-const Ninjas = () => {
+const Ninjas = ({ninjas}) => {
     return ( 
         <div>
             {/* <Navbar/> */}
