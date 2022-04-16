e648ba838ac60f5a51aa5c904b2b8646538cf5d8
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 01:37:23 +0300

    define paths

--- nextjs/ninjalist/pages/ninjas/[id].js
+++ nextjs/ninjalist/pages/ninjas/[id].js
@@ -1,3 +1,16 @@
+export const getStaticPaths =async() =>{
+    const res= await fetch('https://jsonplaceholder.typicode.com/users');
+    //data from json 
+    const data= await res.json();
+    //individual paths 
+    const paths =data.map(ninja=>{
+        return{// array of objects
+            // paths:[ { params :{id :}}]
+            params:{id :ninja.id.toString() }
+        }
+    })
+   
+}
 const Details = () => {
     return ( 
         <div>
