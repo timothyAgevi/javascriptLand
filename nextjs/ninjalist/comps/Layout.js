5e335eb89bf6886dc3a49bcdc67c475e18238ed1
timothyAgevi <timothyagevi@gmail.com>
2022-04-16 21:40:48 +0300

    import link to index.js

--- nextjs/ninjalist/comps/Layout.js
+++ nextjs/ninjalist/comps/Layout.js
@@ -1,5 +1,5 @@
 import Footer from "./footer";
-
+import Navbar from "./Navbar";
 const Layout = ({children}) => {
     return (  
      <div className="content">
--- nextjs/ninjalist/pages/index.js
+++ nextjs/ninjalist/pages/index.js
@@ -3,18 +3,18 @@ import Image from 'next/image'
 import Footer from '../comps/footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+     
       <h1> Homepage</h1>
       <p>lorem nfgjlkgjsd;fS;lgjklkjlkfnkl  </p>
       <p> loremfkgjdbfjksfklasj</p>
       <Link href="/ninjas">
         <a> See Ninjas Listing</a>
       </Link>
-      <Footer/>
+     
     </div>
   )
 }
