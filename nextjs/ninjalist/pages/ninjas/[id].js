04bb127573f91d3ee5a172120916f79d01d9b804
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 18:22:48 +0300

    complete dynamic routes

--- nextjs/ninjalist/pages/ninjas/[id].js
+++ nextjs/ninjalist/pages/ninjas/[id].js
@@ -1,36 +1,40 @@
-export const getStaticPaths =async() =>{//specify dynamic routes to pre-render pages based on data
-    const res= await fetch('https://jsonplaceholder.typicode.com/users');
-    //data from json 
-    const data= await res.json();
-    //individual paths 
-    const paths =data.map(ninja=>{
-        return{// array of objects
-            // paths:[ { params :{id :}}]
-            params:{id :ninja.id.toString() }
-        }
-    })
-    return{
-        paths,
-        fallback:false
-    }
-    //get data for individual ninja
-   export const getStaticProps=async (context)=>{ //context object attaches evey ninja id
-    const id = context.params.id;
-    //fetch request to each id
-    const res= await fetch('https://jsonplaceholder.typicode.com/users' +id);
+//set dynamic routes
+export const getStaticPaths = async () => {//specifies dynamic path to prerendered components
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
-
-    return{
-        props:{ninja:data}
+  
+    // map data to an array of path objects with params (id)
+    const paths = data.map(ninja => {
+      return {
+        params: { id: ninja.id.toString() }
+      }
+    })
+  
+    return {
+      paths,
+      fallback: false
     }
-   }
-}
-const Details = ({ninja}) => {
-    return ( 
-        <div>
-            <h1>Details Page</h1>
-        </div>
-     );
-}
- 
-export default Details;
+  }
+  //get details for single item
+  export const getStaticProps = async (context) => { //context attaches ninja.id
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await res.json();
+  
+    return {
+      props: { ninja: data }
+    }
+  }
+  
+  const Details = ({ ninja }) => {
+    return (
+      <div>
+        <h1>{ ninja.name }</h1>
+        <p>{ ninja.email }</p>
+        <p>{ ninja.website }</p>
+        <p>{ ninja.address.city }</p>
+      </div>
+    );
+  }
+  
+  export default Details;
