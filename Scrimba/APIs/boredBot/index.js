70c417d1fd6bf3f41c2bee5628583fc615e6b0fe
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 23:13:02 +0300

    add textContent to bored bot

--- Scrimba/APIs/boredBot/index.js
+++ Scrimba/APIs/boredBot/index.js
@@ -1,8 +1,8 @@
-fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response => response.json())
-    .then(data =>{ 
-        console.log(data)
-        document.getElementById("image-container").innerHTML=
-        `<img src= "${data.message}" />
-        `
-    })
+fetch( 'https://apis.scrimba.com/bored/api/activity')
+   .then( response=>response.json())
+   .then(data =>{
+       console.log(data)
+       document.getElementById("activity-name").textContent=
+          data.activity
+      
+   })
