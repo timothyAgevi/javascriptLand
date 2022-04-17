96487641039b45fb701d503f51bd51d18098c728
timothyAgevi <timothyagevi@gmail.com>
2022-04-18 01:38:29 +0300

    update title style

--- Scrimba/APIs/boredBot/index.js
+++ Scrimba/APIs/boredBot/index.js
@@ -3,8 +3,8 @@ fetch( 'https://apis.scrimba.com/bored/api/activity')
    .then( response=>response.json())
    .then(data =>{
        console.log(data)
-       document.getElementById("activity").textContent=
-          data.activity
+       document.getElementById("activity").textContent=data.activity
+       document.getElementById("title").textContent ="🦾 HappyBot🦿"
       
    })
 })
