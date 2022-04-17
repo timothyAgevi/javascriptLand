899ccb7f714096eaa19c4cfa3b5e41af25a84608
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 15:11:53 +0300

    update 404page

--- nextjs/ninjalist/pages/404.js
+++ nextjs/ninjalist/pages/404.js
@@ -14,7 +14,7 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <h1>Ooops...</h1>
-      <h2>That page cannot be found :(</h2>
+      <h2>That page cannot be found :</h2>
       <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
     </div>
   );
