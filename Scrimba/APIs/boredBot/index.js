1d01e12d33d5b9a36da5f003eb0a01a9d54b6398
timothyAgevi <timothyagevi@gmail.com>
2022-04-18 01:35:00 +0300

    extra styling comments

--- Scrimba/APIs/boredBot/index.js
+++ Scrimba/APIs/boredBot/index.js
@@ -1,3 +1,4 @@
+document.getElementById("get-activity").addEventListener("click",function(){
 fetch( 'https://apis.scrimba.com/bored/api/activity')
    .then( response=>response.json())
    .then(data =>{
@@ -6,6 +7,7 @@ fetch( 'https://apis.scrimba.com/bored/api/activity')
           data.activity
       
    })
+})
 //    When the button is clicked, call out to the Bored API
 //    (URL: https://apis.scrimba.com/bored/api/activity)
 //    and replace the h4 with the activity from the API
