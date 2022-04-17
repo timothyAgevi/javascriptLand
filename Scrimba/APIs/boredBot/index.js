38fdfc6fce0ecf4199d7cf678e90b1aa53f66d27
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 23:50:13 +0300

    difference between innerHTML ans textContent

--- Scrimba/APIs/boredBot/index.js
+++ Scrimba/APIs/boredBot/index.js
@@ -2,10 +2,12 @@ fetch( 'https://apis.scrimba.com/bored/api/activity')
    .then( response=>response.json())
    .then(data =>{
        console.log(data)
-       document.getElementById("activity-name").textContent=
+       document.getElementById("activity").textContent=
           data.activity
       
    })
 //    When the button is clicked, call out to the Bored API
 //    (URL: https://apis.scrimba.com/bored/api/activity)
 //    and replace the h4 with the activity from the API
+
+//note innerHTML add elements that were not there before. textContent modifies existing content.
