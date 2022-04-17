28304aef5d67c91eb7394d7e39be71a65428c0a2
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 23:41:33 +0300

    instruction to implement in button click

--- Scrimba/APIs/boredBot/index.js
+++ Scrimba/APIs/boredBot/index.js
@@ -6,3 +6,6 @@ fetch( 'https://apis.scrimba.com/bored/api/activity')
           data.activity
       
    })
+//    When the button is clicked, call out to the Bored API
+//    (URL: https://apis.scrimba.com/bored/api/activity)
+//    and replace the h4 with the activity from the API
