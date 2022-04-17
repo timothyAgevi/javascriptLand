4f89b70a383572f4cf946e5a90fc55ee5175d3ac
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 15:57:11 +0300

    getStaticProps

--- nextjs/ninjalist/pages/ninjas/index.js
+++ nextjs/ninjalist/pages/ninjas/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import styles from '../../styles/Ninjas.module.css'
 
 //getStaticProps only runs at build time,dont write browser code here
-export const getStaticProps=async ()=>{
+export const getStaticProps=async ()=>{ //fetch data at build time
     // res object of the fetch request
     const res= await fetch('https://jsonplaceholder.typicode.com/users');
     //data from json 
