9eb66d7f2e7f13152979eeb8d48ddee28c4bd48a
timothyAgevi <timothyagevi@gmail.com>
2022-04-17 22:56:58 +0300

    add fetched img to DOM

--- Scrimba/APIs/boredBot/index.js
+++ Scrimba/APIs/boredBot/index.js
@@ -1,3 +1,8 @@
 fetch("https://dog.ceo/api/breeds/image/random")
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data =>{ 
+        console.log(data)
+        document.getElementById("image-container").innerHTML=
+        `<img src= "${data.message}" />
+        `
+    })
