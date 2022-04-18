a1a2e1d437bc8fdac2928482967c07ec2a9f9b44
timothyAgevi <timothyagevi@gmail.com>
2022-04-18 17:50:23 +0300

    GET Posts from api

--- Scrimba/APIs/blogSpace/index.js
+++ Scrimba/APIs/blogSpace/index.js
@@ -4,6 +4,8 @@
 // Endpoint: /posts
 //limit to 5 items
 // fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+
+//GET Posts from api
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then( response=> response.json())
   .then(data =>{    
