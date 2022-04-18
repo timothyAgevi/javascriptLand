117223a3acadf652be1e6b8c9048c9b62d2593ca
timothyAgevi <timothyagevi@gmail.com>
2022-04-18 13:04:01 +0300

    get list of posts

--- Scrimba/APIs/blogSpace/index.js
+++ Scrimba/APIs/blogSpace/index.js
@@ -1,2 +1,6 @@
-fetch('https://apis.scrimba.com/jsonplaceholder/posts')
+// GET a list of blog posts from the JSON Placeholder API.
+ 
+// BaseURL: https://apis.scrimba.com/jsonplaceholder/
+// Endpoint: /posts
+fetch('https://apis.scrimba.com/jsonplaceholder/posts',{method :"GET"})
   .then()
