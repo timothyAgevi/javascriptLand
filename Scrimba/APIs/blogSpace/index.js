86e9be9558991fa8b9e05d00c99d3569e4f34ef6
timothyAgevi <timothyagevi@gmail.com>
2022-04-18 14:34:00 +0300

    horizontal rule on each post

--- Scrimba/APIs/blogSpace/index.js
+++ Scrimba/APIs/blogSpace/index.js
@@ -7,12 +7,13 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then( response=> response.json())
   .then(data =>{    
-    const postArr =data.slice(0,3)
-    let html =""
+    const postArr =data.slice(0,3)//containg post arry
+    let html =""//variable to store postArray on DOM
     for(let post of postArr){
       html+=`
       <h3> ${post.title}<h/3>
       <p> ${post.body}</p>
+      <hr/>
       `
     }
     document.getElementById("blog-list").innerHTML=html
