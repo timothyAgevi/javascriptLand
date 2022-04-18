79a349305577623b3e1781fe3194522460737241
timothyAgevi <timothyagevi@gmail.com>
2022-04-18 23:42:15 +0300

    get new card from Api

--- Scrimba/APIs/War/index.js
+++ Scrimba/APIs/War/index.js
@@ -1,4 +1,11 @@
 // Challenge: Add a button that, when clicked, gets a new deck of cards from the deckofcards API
  
 //  URL: https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
- 
+const button= document.getElementById("war_btn")
+
+ button.addEventListener('click',function(){
+   
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(response=>response.json())
+    .then( data =>console.log(data))
+ })
