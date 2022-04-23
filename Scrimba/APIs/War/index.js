360f714992d90e8812d19ab07d49e95f85fc2a10
timothyAgevi <timothyagevi@gmail.com>
2022-04-24 00:41:12 +0300

    fetch cards

--- Scrimba/APIs/War/index.js
+++ Scrimba/APIs/War/index.js
@@ -1,11 +1,31 @@
-// Challenge: Add a button that, when clicked, gets a new deck of cards from the deckofcards API
- 
-//  URL: https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
-const button= document.getElementById("war_btn")
+let deckId
 
- button.addEventListener('click',function(){
-   
+function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-    .then(response=>response.json())
-    .then( data =>console.log(data))
- })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+            document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} />
+                <img src=${data.cards[1].image} />
+            `
+        })
+})
+/**
+ * Challenge:
+ * 
+ * Display the images of the 2 cards you drew in the browser.
+ * Probably best to use `innerHTML` to insert a couple <img> elements
+ * on the page.
+ */
