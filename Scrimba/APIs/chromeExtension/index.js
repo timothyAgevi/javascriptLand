cf63642a020b411c15bd57335d8c786073d41a3f
timothyAgevi <timothyagevi@gmail.com>
2022-04-25 13:30:42 +0300

    display author

--- Scrimba/APIs/chromeExtension/index.js
+++ Scrimba/APIs/chromeExtension/index.js
@@ -20,8 +20,8 @@
   .then(res => res.json())
   .then(data => {
       document.body.style.backgroundImage = `url(${data.urls.full})`
-      document.getElementById("author").textContent = data.user.name;
-      
+      document.getElementById("author").textContent = `by ${data.user.name}`;
+
   })
 
      
