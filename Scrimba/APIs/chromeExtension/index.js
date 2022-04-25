f3640e01502587b2d7efb7dd3912c80a3e4853ac
timothyAgevi <timothyagevi@gmail.com>
2022-04-25 15:34:00 +0300

    add cypto data

--- Scrimba/APIs/chromeExtension/index.js
+++ Scrimba/APIs/chromeExtension/index.js
@@ -38,8 +38,14 @@
         <img src=${data.image.small} />
         <span>${data.name}</span>
     `
+    document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
     })
     .catch(err => console.error(err))
+
 /**
  * {
 	id: "KMn4VEeEPR8",
