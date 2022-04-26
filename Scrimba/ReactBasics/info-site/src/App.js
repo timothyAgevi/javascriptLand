2f7f7441994f3530b0a91dbdf56eb2aeb3d7abd3
timothyAgevi <timothyagevi@gmail.com>
2022-04-26 14:30:12 +0300

    correct app.js

--- Scrimba/ReactBasics/info-site/src/App.js
+++ Scrimba/ReactBasics/info-site/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+// import Navbar from "./components/Navbar"
+// import Main from "./components/Main"
 
 export default function App() {
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            {/* <Navbar /> */}
+            {/* <Main /> */}
         </div>
     )
 }
