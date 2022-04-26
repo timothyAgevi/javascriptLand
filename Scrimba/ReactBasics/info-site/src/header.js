3d5fdd57049440aca5de656b598d824da14440e5
timothyAgevi <timothyagevi@gmail.com>
2022-04-26 18:53:02 +0300

    set header as separate component

---
+++ Scrimba/ReactBasics/info-site/src/header.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+ export default function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./logo192.png" width="40px"  alt="React Logo" className="nav-logo" />
+                <ul className="nav-items">
+                      <li>Pricing</li>
+                      <li>About</li>
+                      <li>Contact</li>
+                  </ul>
+            </nav>
+        </header>
+    )
+  }
