289b714a51ef598f433b55113212754777841b04
timothyAgevi <timothyagevi@gmail.com>
2022-04-26 19:02:51 +0300

    complete components

--- Scrimba/ReactBasics/info-site/src/App.js
+++ Scrimba/ReactBasics/info-site/src/App.js
@@ -15,19 +15,7 @@ function Footer() {
   )
 }
 
-function MainContent() {
-  return (
-      <div>
-          <h1>Reasons I'm excited to learn React</h1>
-          <ol>
-              <li>It's a popular library, so I'll be 
-              able to fit in with the cool kids!</li>
-              <li>I'm more likely to get a job as a developer
-              if I know React</li>
-          </ol>
-      </div>
-  )
-}
+
 
 function Page() {
   return (
