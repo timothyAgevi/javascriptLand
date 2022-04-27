11c9e7651649b6e55513ec137ec55d736742a16f
timothyAgevi <timothyagevi@gmail.com>
2022-04-27 16:10:54 +0300

    add box sizing

--- Scrimba/ReactBasics/info-site/src/App.js
+++ Scrimba/ReactBasics/info-site/src/App.js
@@ -1,17 +1,12 @@
-// import logo from './logo.svg';
 import './App.css';
 import React from "react"
-// import Navbar from "./components/Navbar"
-// import Main from "./components/Main"
+
 import ReactDOM from "react-dom"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
 import Main from "./components/Main"
 // Scrimba/ReactBasics/info-site/src/Main.js
-
-
-
 
 function App() {
   return (
