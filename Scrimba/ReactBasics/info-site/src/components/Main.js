3dae6ff17ec58779e40624d78868c5b4a327ceab
timothyAgevi <timothyagevi@gmail.com>
2022-04-27 14:58:53 +0300

    update main component

--- Scrimba/ReactBasics/info-site/src/components/Main.js
+++ Scrimba/ReactBasics/info-site/src/components/Main.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React from "react"
 
 export default function Main() {
     return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-        </div>
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
-  }
+}
+  
