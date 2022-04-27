28e9782177d4abbf7ea12d835e6ebe0837a5ebed
timothyAgevi <timothyagevi@gmail.com>
2022-04-27 14:58:13 +0300

    update Navbar

--- Scrimba/ReactBasics/info-site/src/components/Navbar.js
+++ Scrimba/ReactBasics/info-site/src/components/Navbar.js
@@ -1,10 +1,12 @@
 
 import React from "react"
-
+import reactlogo from "../images/logo.svg"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/react-icon-small.png" className="nav--icon" />
+            <img src={reactlogo} 
+            alt="react Logo"
+            className="nav--icon"/>
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
